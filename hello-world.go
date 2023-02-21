cc0615ca5cfd4bd9050f987fb3cd1593020436fa
rudiney <rudiney1h@gmail.comm>
2023-02-21 20:26:04 -0300

    Link of Requirement

--- hello-world.go
+++ hello-world.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+//requirement:
+//https://plataforma.fullcycle.com.br/courses/250/168/110/conteudos?projeto=51&fase=248
+
 func main() {
 	fmt.Println("Full Cycle Rocks!!")
 }
